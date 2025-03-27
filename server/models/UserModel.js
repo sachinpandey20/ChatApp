dd522a882d5213ff0c20ed320aec7e3695df197e
Sachin Pandey <sachipandey19.sp@gmail.com>
2025-03-27 23:55:30 +0530

    Backend Work

---
+++ server/models/UserModel.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import { genSalt, hash } from "bcrypt";
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    requires: [true, "Email is Requires"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is Required."],
+  },
+  firstName: {
+    type: String,
+    required: false,
+  },
+  lastName: {
+    type: String,
+    required: false,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+  colors: {
+    type: Number,
+    required: false,
+  },
+  profileSetup: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  const salt = await genSalt();
+  this.password = await hash(this.password, salt);
+  next();
+});
+
+const User = mongoose.model("Users", userSchema);
+
+export default User;
